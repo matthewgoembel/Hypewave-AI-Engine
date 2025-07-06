0983d06a49342103811af1abab44c07726c949ea
MattyGPu <mgoembel1@gmail.com>
2025-07-06 16:42:20 -0400

    Update create_session.py
    create session

--- create_session.py
+++ create_session.py
@@ -3,5 +3,5 @@ from telethon.sync import TelegramClient
 api_id = 24594733
 api_hash = '3617efeb2604751e0041dd4b722774c4'
 
-with TelegramClient("fresh_session", api_id, api_hash) as client:
-    print("Session created")
+client = TelegramClient("fresh_session", api_id, api_hash)
+client.start()  # This is what prompts for your phone and code
