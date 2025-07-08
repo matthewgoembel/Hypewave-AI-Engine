50f0dcb7bc162de11725b9b01d9581b636e01142
MattyGPu <mgoembel1@gmail.com>
2025-07-07 23:30:51 -0400

    dependei sinstall

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-echo "📁 Installing Node dependencies..."
-cd hypewave-screenshot
-npm install
-
-cd ..
-
 echo "🚀 Starting Hypewave AI backend..."
 uvicorn api:app --host 0.0.0.0 --port 10000
